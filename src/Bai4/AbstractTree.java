1993aecb4704bbc9452ccd7ef0ebede8bf21a663
Minhkun912 <minhnguyenduc506@gmail.com>
2023-03-24 08:31:56 +0700

    first commit

---
+++ src/Bai4/AbstractTree.java
@@ -0,0 +1,8 @@
+package Bai4;
+
+public abstract class AbstractTree<E> implements Tree<E>{
+    @Override
+    /** Inorder traversal from the root*/
+    public void inorder() {
+    }
+}
---
+++ src/Bai4/Tree.java
@@ -0,0 +1,11 @@
+package Bai4;
+
+public interface Tree<E> {
+
+    boolean insert(E e);
+
+
+    void inorder();
+
+    int getSize();
+}
